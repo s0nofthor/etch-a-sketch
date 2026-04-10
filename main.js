d4afe88b3a4507d3548acd34722cac88653f1017
s0nofthor <larsthomast@gmail.com>
2026-04-10 18:17:25 -0400

    Adapt screen for thin monitors

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+const cont = document.getElementById('container');
+if (window.innerHeight > window.innerWidth) {
+    cont.className = 'skinny';
+} else if (window.innerWidth > window.innerHeight) {
+    cont.className = 'fat';
+}
+addEventListener('resize', function() {
+    if (this.window.innerHeight > this.window.innerWidth) {
+        cont.className = 'skinny';
+    } else if (this.window.innerWidth > this.window.innerHeight) {
+        cont.className = 'fat';
+    }
+})
+
 //Make Tron's home
 for (i=0; i<256; i++) {
     const tile = document.createElement('div');
